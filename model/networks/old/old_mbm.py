c172012297500654b9051df81afaba3d9167347d
lcalem <laura.calem@gmail.com>
2019-05-15 16:24:38 +0200

    [debug] new decoder

--- model/networks/old/old_mbm.py
+++ model/networks/old/old_mbm.py
@@ -10,6 +10,7 @@ from model.losses import reconstruction_loss
 
 from model.networks import BaseModel
 from model.networks.old_decoder import DecoderModel
+# from model.networks.decoder_model import DecoderModel
 from model.networks.old_pose import PoseModel
 
 
@@ -28,26 +29,24 @@ class MultiBranchModel(BaseModel):
         self.n_joints = n_joints
         self.n_blocks = nb_pose_blocks
         self.reception_kernel_size = reception_kernel_size
-        
         self.verbose = True
+        
+        self.zp_depth = 1024
 
         BaseModel.__init__(self)
 
     def build(self):
-        self.decoder_model = self.build_decoder_model((8, 8, 2048))  # i.e. 2048 for the regular model
+        self.build_everything()
         
         inp = Input(shape=self.input_shape)
-
-        # encoders
-        time_1 = time.time()
-        z_a = self.appearance_encoder(inp)
-        time_2 = time.time()
-        z_p = self.pose_encoder(inp)
-        time_3 = time.time()
-
-        print("Build E_a %s, build E_p %s" % (time_2 - time_1, time_3 - time_2))
-        print(type(z_a), type(z_p))
-        print("Shape z_a %s" % str(z_a.shape))
+        self.log("Input shape %s" % str(inp.shape))
+        
+        z_a = self.appearance_model(inp)
+        z_p = self.pose_model(inp)
+        # pose_outputs = self.pose_model(inp)
+        # poses, z_p = self.check_pose_output(pose_outputs)
+        
+        # self.log("Shape z_a %s, shape z_p %s" % (str(z_a.shape), str(z_p.shape)))
 
         # decoder
         concat = self.concat(z_a, z_p)
@@ -55,6 +54,7 @@ class MultiBranchModel(BaseModel):
         i_hat = self.decoder_model(concat)
 
         outputs = [i_hat]
+        # outputs.extend(poses)
         outputs.extend(z_p)
         self.model = Model(inputs=inp, outputs=outputs)
         print("Outputs shape %s" % self.model.output_shape)
@@ -62,26 +62,20 @@ class MultiBranchModel(BaseModel):
         ploss = [pose_loss()] * len(z_p)
         losses = [reconstruction_loss()]
         losses.extend(ploss)
-        
+        # loss = mean_squared_error
         self.model.compile(loss=losses, optimizer=RMSprop(lr=self.start_lr))
         self.model.summary()
         
     def build_everything(self):
-        '''
-        pose model
-        appearance model
-        decoder model
-        '''
         time_1 = time.time()
         self.appearance_model = self.build_appearance_model(self.input_shape)
         time_2 = time.time()
         self.pose_model = self.build_pose_model(self.input_shape)
         time_3 = time.time()
-        self.decoder_model = self.build_decoder_model((8, 8, 2048))  # i.e. 2048 for the regular model
+        self.decoder_model = self.build_decoder_model((16, 16, 1024))  # i.e. 2048 for the regular model
         time_4 = time.time()
         
         self.log("Build E_a %s, build E_p %s, decoder D %s" % (time_2 - time_1, time_3 - time_2, time_4 - time_3))
-        
         
     def build_pose_only(self):
         '''
@@ -96,56 +90,57 @@ class MultiBranchModel(BaseModel):
         ploss = [pose_loss()] * len(z_p)
         self.model.compile(loss=ploss, optimizer=RMSprop(lr=self.start_lr))
         self.model.summary()
-        
-    def appearance_encoder(self, inp):
+
+    def build_appearance_model(self, input_shape):
         '''
         resnet50 for now
         input: 256 x 256 x 3
-        output: 8 x 8 x 2048
+        output: 16 x 16 x 1024
         '''
-        enc_model = ResNet50(include_top=False, weights='imagenet', input_tensor=inp)
-
-        z_a = enc_model.output   # 8 x 8 x 2048
-        return z_a
-
-    def pose_encoder(self, inp):
+        enc_model = ResNet50(include_top=False, weights='imagenet', input_shape=input_shape)
+        output_layer = enc_model.layers[-33]  # index of the 16 x 16 x za_depth activation we want, before the last resnet block
+        assert output_layer.name.startswith('activation')
+        
+        partial_model = Model(inputs=enc_model.inputs, outputs=output_layer.output, name='appearance_model')
+        return partial_model
+    
+    def build_pose_model(self, input_shape, pose_only=False):
         '''
         reception / stacked hourglass
         input: 256 x 256 x 3
-        output: [] x 8
+        output: [(n_joints, dim + 1) * n_blocks, (16, 16, zp_depth)]   (zp_depth = 1024 or 128)
         '''
-        pose_model = PoseModel(inp, self.dim, self.n_joints, self.n_blocks, self.reception_kernel_size).model
-        out = pose_model.output
-
-        return out
+        return PoseModel(input_shape, self.dim, self.n_joints, self.n_blocks, self.reception_kernel_size).model
     
     def build_decoder_model(self, input_shape):
         '''
         from concatenated representations to image reconstruction
-        input: 8 x 8 x 2048 (z_a)
+        input: 16 x 16 x 1024 (z_a and z_p concatenated)    [or 256]
         output: 256 x 256 x 3
         '''
         return DecoderModel(input_shape=input_shape).model
-    
-    def decoder(self, concat):
-        '''
-        from concatenated representations to image reconstruction
-        input: 8 x 8 x 2048 (z_a)
-        output: 256 x 256 x 3
-        '''
-        decoder_model = DecoderModel(input_tensor=concat).model
-        out = decoder_model(concat)
 
-        return out
-    
     def concat(self, z_a, z_p):
         '''
         concat pose and appearance representations before decoding
         input:
             - z_p:
-            - z_a: 8 x 8 x 2048
+            - z_a: 16 x 16 x 1024
         output:
 
-        TODO: This is where the real work should happen
         '''
         return z_a
+    
+    def check_pose_output(self, pose_outputs):
+        '''
+        pose_outputs should be a list of poses + z_p
+        '''
+        assert len(pose_outputs) == self.n_blocks + 1  # + 1 for the z_p (16 x 16 x zp_depth) representation
+        poses = pose_outputs[:-1]
+        z_p = pose_outputs[-1]
+        
+        pose_shapes = [pose.shape.as_list() for pose in poses]
+        assert all([shape == [None, self.n_joints, self.dim + 1] for shape in pose_shapes]), 'pose shapes are weird %s' % str(pose_shapes)
+        assert z_p.shape.as_list() == [None, 16, 16, self.zp_depth], 'z_p shape not as expected %s' % str(z_p.shape.as_list())
+        
+        return poses, z_p
